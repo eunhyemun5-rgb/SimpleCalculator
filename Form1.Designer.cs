60b000dd83b83cf59f5176cc70e662b461d9e3cd
은혜 문 <https://github.com/eunhyemun5-rgb>
2026-03-26 12:18:43 +0900

    과제1 실습완료

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,337 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("맑은 고딕", 15F);
+            textBox1.Location = new Point(81, 150);
+            textBox1.Margin = new Padding(5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(582, 61);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ButtonHighlight;
+            label1.Font = new Font("맑은 고딕", 24F);
+            label1.Location = new Point(113, 35);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(528, 86);
+            label1.TabIndex = 1;
+            label1.Text = "Simple Caculator";
+            label1.Click += label1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("맑은 고딕", 15F);
+            textBox2.Location = new Point(81, 221);
+            textBox2.Margin = new Padding(5);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(582, 61);
+            textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 15F);
+            button1.Location = new Point(81, 310);
+            button1.Margin = new Padding(5);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 83);
+            button1.TabIndex = 3;
+            button1.Text = "CE";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("맑은 고딕", 15F);
+            button2.Location = new Point(241, 313);
+            button2.Margin = new Padding(5);
+            button2.Name = "button2";
+            button2.Size = new Size(109, 80);
+            button2.TabIndex = 4;
+            button2.Text = "C";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("맑은 고딕", 15F);
+            button3.Location = new Point(400, 316);
+            button3.Margin = new Padding(5);
+            button3.Name = "button3";
+            button3.Size = new Size(113, 77);
+            button3.TabIndex = 5;
+            button3.Text = "del";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(561, 319);
+            button4.Margin = new Padding(5);
+            button4.Name = "button4";
+            button4.Size = new Size(102, 77);
+            button4.TabIndex = 6;
+            button4.Text = "÷";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.ForeColor = SystemColors.MenuHighlight;
+            button5.Location = new Point(81, 419);
+            button5.Name = "button5";
+            button5.Size = new Size(114, 75);
+            button5.TabIndex = 7;
+            button5.Text = "7";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.ForeColor = SystemColors.MenuHighlight;
+            button6.Location = new Point(241, 419);
+            button6.Name = "button6";
+            button6.Size = new Size(109, 75);
+            button6.TabIndex = 8;
+            button6.Text = "8";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.ForeColor = SystemColors.MenuHighlight;
+            button7.Location = new Point(400, 419);
+            button7.Name = "button7";
+            button7.Size = new Size(113, 75);
+            button7.TabIndex = 9;
+            button7.Text = "9";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(561, 419);
+            button8.Name = "button8";
+            button8.Size = new Size(102, 75);
+            button8.TabIndex = 10;
+            button8.Text = "X";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.ForeColor = SystemColors.MenuHighlight;
+            button9.Location = new Point(81, 517);
+            button9.Name = "button9";
+            button9.Size = new Size(114, 69);
+            button9.TabIndex = 11;
+            button9.Text = "4";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.ForeColor = SystemColors.MenuHighlight;
+            button10.Location = new Point(241, 517);
+            button10.Name = "button10";
+            button10.Size = new Size(109, 69);
+            button10.TabIndex = 12;
+            button10.Text = "5";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.ForeColor = SystemColors.MenuHighlight;
+            button11.Location = new Point(400, 517);
+            button11.Name = "button11";
+            button11.Size = new Size(113, 69);
+            button11.TabIndex = 13;
+            button11.Text = "6";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(561, 517);
+            button12.Name = "button12";
+            button12.Size = new Size(102, 69);
+            button12.TabIndex = 14;
+            button12.Text = "-";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.ForeColor = SystemColors.MenuHighlight;
+            button13.Location = new Point(81, 611);
+            button13.Name = "button13";
+            button13.Size = new Size(114, 65);
+            button13.TabIndex = 15;
+            button13.Text = "1";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.ForeColor = SystemColors.MenuHighlight;
+            button14.Location = new Point(241, 611);
+            button14.Name = "button14";
+            button14.Size = new Size(109, 65);
+            button14.TabIndex = 16;
+            button14.Text = "2";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.ForeColor = SystemColors.MenuHighlight;
+            button15.Location = new Point(400, 611);
+            button15.Name = "button15";
+            button15.Size = new Size(113, 65);
+            button15.TabIndex = 17;
+            button15.Text = "3";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += button15_Click;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(561, 611);
+            button16.Name = "button16";
+            button16.Size = new Size(102, 65);
+            button16.TabIndex = 18;
+            button16.Text = "+";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += button16_Click;
+            // 
+            // button17
+            // 
+            button17.ForeColor = SystemColors.InfoText;
+            button17.Location = new Point(81, 706);
+            button17.Name = "button17";
+            button17.Size = new Size(114, 65);
+            button17.TabIndex = 19;
+            button17.Text = "+/-";
+            button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            button18.Font = new Font("맑은 고딕", 15F);
+            button18.ForeColor = SystemColors.MenuHighlight;
+            button18.Location = new Point(236, 706);
+            button18.Margin = new Padding(5);
+            button18.Name = "button18";
+            button18.Size = new Size(114, 65);
+            button18.TabIndex = 20;
+            button18.Text = "0";
+            button18.UseVisualStyleBackColor = true;
+            button18.Click += button18_Click;
+            // 
+            // button19
+            // 
+            button19.Location = new Point(400, 706);
+            button19.Name = "button19";
+            button19.Size = new Size(113, 65);
+            button19.TabIndex = 21;
+            button19.Text = ".";
+            button19.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            button20.Location = new Point(561, 706);
+            button20.Name = "button20";
+            button20.Size = new Size(102, 65);
+            button20.TabIndex = 22;
+            button20.Text = "=";
+            button20.UseVisualStyleBackColor = true;
+            button20.Click += button20_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(14F, 32F);
+            AutoScaleDimensions = new SizeF(23F, 54F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1484, 753);
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(757, 880);
+            Controls.Add(button20);
+            Controls.Add(button19);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Font = new Font("맑은 고딕", 15F);
+            Margin = new Padding(5);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button button20;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,14 +2,172 @@ namespace SimpleCalculator
 {
     public partial class Form1 : Form
     {
+        int storedNumber = 0;
+        string currentOperator = "";
+        bool isNewInput = true;
         public Form1()
         {
+
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "0";
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                storedNumber = int.Parse(textBox1.Text);
+                currentOperator = "+";
+                textBox1.Text = ""; // 다음 숫자를 입력받기 위해 텍스트박스를 비워줍니다.
+            }
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            int secondNumber = int.Parse(textBox1.Text);
+            if (currentOperator == "+")
+            {
+                int result = storedNumber + secondNumber;
+
+                textBox1.Text = storedNumber.ToString() + " + " + secondNumber.ToString() + " =";
+                textBox2.Text = result.ToString();
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "1";
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "2";
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "3";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "4";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "5";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "6";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "7";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "8";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "9";
+            }
         }
     }
 }
